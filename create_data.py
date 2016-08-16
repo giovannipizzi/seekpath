Some fixes - the visualizer was not scaling by 2pi... Now I also (optionally, off by default) show the explicit kpoints of the path on the 3d visualizer

create_data.py
@@ -7,7 +7,7 @@ import numpy as np
 import ase, ase.io
 
 from brillouinzone import brillouinzone
-from kpaths3d import hkot 
+from kpaths3d import hkot, get_explicit_k_path
 
 # If True, does not expect a case from command line, but generates
 # all the available ones
@@ -29,7 +29,8 @@ def get_json_for_visualizer(case, with_inv):
     res = hkot.get_path(system, with_time_reversal=False) 
 
     real_lattice = res['primitive_lattice']
-    rec_lattice = np.linalg.inv(real_lattice).T
+    #rec_lattice = np.linalg.inv(real_lattice).T # Missing 2pi!
+    rec_lattice = np.array(hkot.tools.get_reciprocal_cell_rows(real_lattice))
     b1, b2, b3 = rec_lattice
 
 
@@ -45,6 +46,20 @@ def get_json_for_visualizer(case, with_inv):
     response['kpoints'] = {k: (v[0] * b1 + v[1] * b2 + v[2] * b3).tolist()
         for k,v in res['point_coords'].iteritems()}
     response['path'] = res['path']
+
+    # It should use the same logic, so give the same cell as above
+    res_explicit = get_explicit_k_path(system, with_time_reversal=False) 
+    for k in ['kpoints_rel', 'kpoints_linearcoord', 'kpoints_labels', 
+            'kpoints_abs', 'segments']:
+        new_k = "explicit_{}".format(k)
+        if isinstance(res_explicit[k], np.ndarray):
+            response[new_k] = res_explicit[k].tolist()
+        else:
+            response[new_k] = res_explicit[k]
+
+    if np.sum(np.abs(np.array(res_explicit['reciprocal_primitive_lattice']) - 
+        np.array(res['reciprocal_primitive_lattice']))) > 1.e-7:
+        raise AssertionError("Got different reciprocal cells...")
 
     return response
 