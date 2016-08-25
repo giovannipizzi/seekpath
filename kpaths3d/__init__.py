47fc5026c57deb2e14064cc88d3c926c84c954e0
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2016-08-25 16:49:18 +0200

    Implemented also functions and tests to get the 'implicit' path from a recipe name, also with the AiiDA wrappers.

--- kpaths3d/__init__.py
+++ kpaths3d/__init__.py
@@ -131,6 +131,105 @@ def get_aiida_structure_from_tuple(structure_tuple, kind_info=None, kinds=None):
     return out_structure
 
 
+def get_path(structure, with_time_reversal=True, recipe='hkot',
+             threshold=1.e-7):
+    """
+    Return the kpoint path information for band structure given a 
+    crystal structure, using the paths from the chosen recipe/reference.
+
+    If you use this module, please cite both the AiiDA paper for 
+    the implementation::
+
+      G. Pizzi et al., Comp. Mat. Sci. 111, 218 (2016),
+
+    and the paper of the corresponding recipe (see parameter below).
+
+    :param structure: The crystal structure for which we want to obtain
+        the suggested path. It should be a tuple in the format
+        accepted by spglib: (cell, positions, numbers), where 
+        (if N is the number of atoms):
+
+        - cell is a 3x3 list of floats (cell[0] is the first lattice 
+          vector, ...)
+        - positions is a Nx3 list of floats with the atomic coordinates
+          in scaled coordinates (i.e., w.r.t. the cell vectors)
+        - numbers is a length-N list with integers identifying uniquely
+          the atoms in the cell (e.g., the Z number of the atom, but 
+          any other positive non-zero integer will work - e.g. if you
+          want to distinguish two Carbon atoms, you can set one number
+          to 6 and the other to 1006)
+
+    :param with_time_reversal: if False, and the group has no inversion 
+        symmetry, additional lines are returned as described in the HKOT
+        paper.
+
+    :param recipe: choose the reference publication that defines the special
+       points and paths.
+       Currently, the following value is implemented:
+       'hkot': HKOT paper: 
+       Y. Hinuma, Y Kumagai, F. Oba, I. Tanaka, Band structure diagram 
+       paths based on crystallography, arXiv:1602.06402 (2016)
+
+   :param threshold: the threshold to use to verify if we are in 
+        and edge case (e.g., a tetragonal cell, but a==c). For instance, 
+        in the tI case, if abs(a-c) < threshold, a EdgeCaseWarning is issued. 
+        Note that depending on the bravais lattice, the meaning of the 
+        threshold is different (angle, length, ...)
+
+    :return: a dictionary with the following 
+      keys:
+
+        - point_coords: a dictionary with label -> float coordinates
+        - path: a list of length-2 tuples, with the labels of the starting
+          and ending point of each label section
+        - has_inversion_symmetry: True or False, depending on whether the
+          input crystal structure has inversion symmetry or not.
+        - augmented_path: if True, it means that the path was
+          augmented with the -k points (this happens if both 
+          has_inversion_symmetry is False, and the user set 
+          with_time_reversal=False in the input)
+        - bravais_lattice: the Bravais lattice string (like 'cP', 'tI', ...)
+        - bravais_lattice_case: the specific case used to define labels and
+          coordinates (like 'cP1', 'tI2', ...)
+        - std_lattice: three real-space vectors for the standard conventional 
+          cell (std_lattice[0,:] is the first vector)
+        - std_positions: fractional coordinates of atoms in the standard 
+          conventional cell 
+        - std_types: list of integer types of the atoms in the standard 
+          conventional cell (typically, the atomic numbers)
+        - primitive_lattice: three real-space vectors for the standard primitive
+          cell (primitive_lattice[0,:] is the first vector)
+        - primitive_positions: fractional coordinates of atoms in the standard 
+          primiitive cell 
+        - primitive_types: list of integer types of the atoms in the standard 
+          conventional cell (typically, the atomic numbers)
+        - reciprocal_primitive_lattice: reciprocal-cell vectors for the 
+          primitive cell (vectors are rows: reciprocal_primitive_lattice[0,:] 
+          is the first vector)
+        - primitive_transformation_matrix: the transformation matrix P between
+          the conventional and the primitive cell 
+        - inverse_primitive_transformation_matrix: the inverse of the matrix P
+          (the determinant is integer and gives the ratio in volume between
+          the conventional and primitive cells)
+        - volume_original_wrt_std: volume ratio of the user-provided cell
+          with respect to the the standard conventional cell 
+        - volume_original_wrt_prim: volume ratio of the user-provided cell
+          with respect to the the standard primitive cell 
+
+    :note: An EdgeCaseWarning is issued for edge cases (e.g. if a==b==c for
+        orthorhombic systems). In this case, still one of the valid cases
+        is picked.
+    """
+    if recipe == 'hkot':
+        from . import hkot
+        res = hkot.get_path(structure, with_time_reversal, threshold)
+
+        return res
+    else:
+        raise ValueError("value for 'recipe' not recognized. The only value "
+            "currently accepted is 'hkot'.")
+
+
 def get_explicit_k_path(structure, with_time_reversal=True,
     reference_distance=0.025, recipe='hkot', 
     threshold=1.e-7):
@@ -145,7 +244,12 @@ def get_explicit_k_path(structure, with_time_reversal=True,
     crystal cell provided in output for all calculations, rather than the
     input 'structure'.
 
-    :todo: check units of reference_distance (and of the reciprocal_primitive_lattice)
+    If you use this module, please cite both the AiiDA paper for 
+    the implementation::
+
+      G. Pizzi et al., Comp. Mat. Sci. 111, 218 (2016),
+
+    and the paper of the corresponding recipe (see parameter below).
 
     :param structure: The crystal structure for which we want to obtain
         the suggested path. It should be a tuple in the format
@@ -195,7 +299,7 @@ def get_explicit_k_path(structure, with_time_reversal=True,
         - primitive_lattice: three real-space vectors for the standard primitive
           cell (primitive_lattice[0,:] is the first vector)
         - primitive_positions: fractional coordinates of atoms in the standard 
-          primiitive cell 
+          primitive cell 
         - primitive_types: list of integer types of the atoms in the standard 
           conventional cell (typically, the atomic numbers)
         - reciprocal_primitive_lattice: reciprocal-cell vectors for the 
