e2bfc3c97e439e892fdc323e4336dd257e07dab0
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2016-09-29 15:43:45 +0200

    Added version number

--- seekpath/__init__.py
+++ seekpath/__init__.py
@@ -6,4 +6,11 @@ Author: Giovanni Pizzi, EPFL (2016)
 
 Licence: MIT License, see LICENSE.txt file
 """
+
+__version__ = "1.0.0"
+__author__ = "Giovanni Pizzi, EPFL"
+__copyright__ = "Copyright (c), 2016, Giovanni Pizzi, EPFL (Theory and Simulation of Materials (THEOS) and National Centre for Computational Design and Discovery of Novel Materials (NCCR MARVEL)), Switzerland."
+__credits__ = ["Yoyo Hinuma"]
+__license__ = "MIT license"
+
 from .getpaths import get_path, get_explicit_k_path
